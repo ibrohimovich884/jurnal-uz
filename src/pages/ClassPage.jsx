d0821fae75a66ac89cca4c8142f4bb4556cd7c52
ibrohimovich884 <bkbekmirzayev@gmail.com>
2025-11-13 20:19:07 +0500

    7a complate

--- src/pages/ClassPage.jsx
+++ src/pages/ClassPage.jsx
@@ -65,7 +65,7 @@ export default function ClassPage() {
 						</ul>
 					</div>
 				))}
-				<p className="beta"><span>Update: 2025.11.13 - 15:47</span></p>
+				<p className="beta"><span>Update: 2025.11.13 - 19:59</span></p>
 			</div>
 		</div>
 	);
